ebd2938d5df8f83da851f3d57300a17db948febd
MidasPark <161546859+MidasPark@users.noreply.github.com>
2025-03-26 18:32:07 +0900

    [#ISSUES-15] News 도메인에 스웨거 & Grpc 기본 세팅 (#18)

---
+++ news/src/main/java/com/salt/news/application/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.salt.news.application.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springNewsOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Your API Title")
+                        .description("Your API Description")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("External Documentation")
+                        .url("https://your-external-docs-url"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
---
+++ news/src/main/java/com/salt/news/port/primary/in/grpc/NewsDataPort.java
@@ -0,0 +1,1 @@
+package com.salt.news.port.primary.in.grpc;
